23cea5f03949b709c009ed6ad1a3fef92de4b488
Rossana Suarez <46635001+roxsross@users.noreply.github.com>
2023-02-02 22:07:19 -0300

    Update docker_getenv.sh

--- docker_getenv.sh
+++ docker_getenv.sh
@@ -11,8 +11,6 @@ fi
 #get reponame
 git_url=$(basename $(git config --get remote.origin.url))
 REPONAME=${git_url/\.git/''}
-AUTOMATION_SHORT=$(cd automation;git rev-parse --short HEAD)
-AUTOMATION_BRANCH_NAME=$(cd automation;git rev-parse --abbrev-ref HEAD)
 GIT_SHORT=$(git rev-parse --short HEAD) 
 GIT_LAST_TAG=$(git tag --sort=committerdate|tail -n 1)  > /dev/null 2>&1
 #get BRANCH_NAME from  GIT
